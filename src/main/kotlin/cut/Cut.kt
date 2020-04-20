8ec257104bd9115c42c538b913389234f27960f6
eugene <judge15rus@mail.ru>
2020-04-20 18:26:48 +0300

    Fix from Microsoft Teams (x2)

--- src/main/kotlin/cut/Cut.kt
+++ src/main/kotlin/cut/Cut.kt
@@ -6,8 +6,6 @@ import java.io.IOException
 class Cut (private val c : Boolean, private val w : Boolean, private val oFile : String,
            private val iFile : String, private val range : String) {
 
-    private val outList = mutableListOf<String>()
-
     @Throws(IOException::class)
 
 
@@ -20,89 +18,93 @@ class Cut (private val c : Boolean, private val w : Boolean, private val oFile :
         }
 
 
-        while (w && text.hasNext()) {
-            cutW(text.next())
+        if (oFile == "") {
+
+            while (w && text.hasNext()) {
+                println(cutW(text.next()))
+            }
+
+            while (c && text.hasNext()) {
+                println(cutC(text.next()))
+            }
+        } else {
+            var skipFirstSpace = true
+            File(oFile).bufferedWriter().use {
+                while (w && text.hasNext()) {
+                    if (skipFirstSpace) {
+                        it.write(cutW(text.next()))
+                        skipFirstSpace = false
+                        continue
+                    }
+                    it.newLine()
+                    it.write(cutW(text.next()))
+                }
+                while (c && text.hasNext()) {
+                    if (skipFirstSpace) {
+                        it.write(cutC(text.next()))
+                        skipFirstSpace = false
+                        continue
+                    }
+                    it.newLine()
+                    it.write(cutC(text.next()))
+                }
+            }
         }
 
-        while (c && text.hasNext()) {
-            cutC(text.next())
-        }
-
-        writer(outList)
 
     }
 
-    private fun cutW (entry: String) {
+    private fun cutW (entry: String): String {
         val lineList = mutableListOf<String>()
         Regex("""[^ ]+""").findAll(entry).forEach { lineList.add(it.value) }
         if (range.matches(Regex("""-\d+"""))) {
             val k = range.drop(1).toInt()
-            if (k >= lineList.size) {
-                outList.add(lineList.joinToString(separator = " "))
+            return if (k >= lineList.size) {
+                lineList.joinToString(separator = " ")
             } else {
-                outList.add(lineList.slice(0 until k).joinToString(separator = " "))
+                lineList.slice(0 until k).joinToString(separator = " ")
             }
 
         } else if (range.matches(Regex("""\d+-"""))) {
             val n = range.dropLast(1).toInt()
-            if (n >= lineList.size) {
-                outList.add("")
+            return if (n >= lineList.size) {
+                ""
             } else {
-                outList.add(lineList.slice(n - 1 until lineList.size).joinToString(separator = " "))
+                lineList.slice(n - 1 until lineList.size).joinToString(separator = " ")
             }
 
         } else {
             val listOfStartEnd = mutableListOf<Int>()
             Regex("""\d+""").findAll(range).forEach { listOfStartEnd.add(it.value.toInt()) }
-            if (listOfStartEnd[1] >= lineList.size) {
-                outList.add(
-                    lineList.slice(
-                        (listOfStartEnd[0] - 1) until lineList.size
-                    ).joinToString(separator = " ")
-                )
-            } else outList.add(
-                lineList.slice(
-                    (listOfStartEnd[0] - 1) until listOfStartEnd[1]
-                ).joinToString(separator = " ")
-            )
+            return if (listOfStartEnd[1] >= lineList.size) {
+                lineList.slice((listOfStartEnd[0] - 1) until lineList.size).joinToString(separator = " ")
+
+            } else lineList.slice(
+                (listOfStartEnd[0] - 1) until listOfStartEnd[1]
+            ).joinToString(separator = " ")
+
         }
 
     }
 
-    private fun cutC (entry: String) {
+    private fun cutC (entry: String): String {
         if (range.matches(Regex("""-\d+"""))) {
             val k = range.drop(1).toInt()
-            if (k >= entry.length) {
-                outList.add(entry)
-            } else outList.add(entry.slice(0 until k))
+            return if (k >= entry.length) {
+                entry
+            } else entry.slice(0 until k)
         } else if (range.matches(Regex("""\d+-"""))) {
             val n = range.dropLast(1).toInt()
-            if (n >= entry.length) {
-                outList.add("")
-            } else outList.add(entry.slice((n - 1) until entry.length))
+            return if (n >= entry.length) {
+                ""
+            } else entry.slice((n - 1) until entry.length)
         } else {
             val listOfStartEnd = mutableListOf<Int>()
             Regex("""\d+""").findAll(range).forEach { listOfStartEnd.add(it.value.toInt()) }
-            if (listOfStartEnd[1] >= entry.length) {
-                outList.add(entry.slice(listOfStartEnd[0] until entry.length))
-            } else outList.add(entry.slice((listOfStartEnd[0] - 1) until listOfStartEnd[1]))
+            return if (listOfStartEnd[1] >= entry.length) {
+                entry.slice(listOfStartEnd[0] until entry.length)
+            } else entry.slice((listOfStartEnd[0] - 1) until listOfStartEnd[1])
         }
     }
-
-private fun writer (outList: List<String>) {
-    if (oFile == "") {
-        for (i in outList) {
-            println(i)
-        }
-    } else {
-        File(oFile).bufferedWriter().use {
-            for (i in 0 until outList.size - 1) {
-                it.write(outList[i])
-                it.newLine()
-            }
-            it.write(outList.last())
-        }
-    }
-}
 
 }
